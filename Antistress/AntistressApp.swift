63ae234c962f27833e0928c3c276b66a02da60fd
C'est une pomme <dmgordienko@gmail.com>
2023-01-03 22:27:15 +0400

    Initial Commit

---
+++ Antistress/AntistressApp.swift
@@ -0,0 +1,17 @@
+//
+//  AntistressApp.swift
+//  Antistress
+//
+//  Created by Дмитрий Гордиенко on 03.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct AntistressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
